70520f84b13662bcc06e421c6be3a917c2a8862f
Irfanmuarif <imuarif570@gmail.com>
2026-01-09 22:58:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -435,8 +435,8 @@ async function exportToPDF() {
                     cellPadding: 3
                 },
                 columnStyles: {
-                    0: { cellWidth: 15, halign: 'center' },
-                    1: { cellWidth: 55 },
+                    0: { cellWidth: 20, halign: 'center' },
+                    1: { cellWidth: 50 },
                     2: { cellWidth: 25, halign: 'right' },
                     3: { cellWidth: 25, halign: 'right' },
                     4: { cellWidth: 30, halign: 'right' }
